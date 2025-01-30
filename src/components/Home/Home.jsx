e51f7552e7ea25dcb31a831ac7c87ba90d39de8e
isha-web1 <isha.web1@gmail.com>
2025-01-30 21:36:20 +0600

    navbar and set theme

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='h-16'>
+            <Navbar/>
+            </div>
+            <h1>this is home page</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/bookmarks/BookMarks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookMarks = () => {
+    return (
+        <div>
+            <h1>this is a bookmarks page</h1>
+        </div>
+    );
+};
+
+export default BookMarks;
